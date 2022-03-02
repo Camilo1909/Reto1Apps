90ce2ed122cb3980179a078d1c98a2a154c2c62f
Camilo1909 <kmilo0919@gmail.com>
2022-03-01 21:03:00 -0500

    Actividades y fragmentos esquema

---
+++ app/src/main/java/com/example/reto1/NavigationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.reto1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stage)
+    }
+}
