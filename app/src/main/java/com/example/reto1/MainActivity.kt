9dfd07ad2b417047480a3b1909ce848b85cd2340
Camilo1909 <kmilo0919@gmail.com>
2022-03-18 17:45:07 -0500

    Camera y gallery

--- app/src/main/java/com/example/reto1/MainActivity.kt
+++ app/src/main/java/com/example/reto1/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.reto1
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var loginBtn:Button
+
+    var allGrant = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +22,30 @@ class MainActivity : AppCompatActivity() {
 
         loginBtn = findViewById(R.id.loginBtn)
 
-
+        requestPermissions(arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ),1)
         loginBtn.setOnClickListener{
-            val intent = Intent(this,NavigationActivity::class.java)
-            startActivity(intent)
+            if (allGrant){
+                val intent = Intent(this,NavigationActivity::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this,"Por favor aceptar todos los permisos",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode==1){
+            for(result in grantResults) {
+                if (result == PackageManager.PERMISSION_DENIED) allGrant = false
+            }
         }
     }
 }
--- app/src/main/java/com/example/reto1/PublicationFragment.kt
+++ app/src/main/java/com/example/reto1/PublicationFragment.kt
@@ -1,17 +1,30 @@
 package com.example.reto1
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.icu.text.DateFormat
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.core.content.FileProvider
 import com.example.reto1.databinding.FragmentPublishBinding
+import java.io.File
 
 class PublicationFragment : Fragment() {
 
     private var _binding: FragmentPublishBinding? = null
     private val binding get() = _binding!!
+
+    private var file:File? = null
 
     var listener: OnNewPublicationListener?=null
 
@@ -23,19 +36,62 @@ class PublicationFragment : Fragment() {
         _binding = FragmentPublishBinding.inflate(inflater,container,false)
         val view = binding.root
 
+        val cameralauncher = registerForActivityResult(StartActivityForResult(), ::onCameraResult)
+        val gallerylauncher = registerForActivityResult(StartActivityForResult(), ::onGalleryResult)
+
+        binding.cameraBtn.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            val context = activity as NavigationActivity
+            file = File("${context.getExternalFilesDir(null)}/photo.png")
+            val uri = FileProvider.getUriForFile(context, context.packageName,file!!)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,uri)
+            Log.e(">>>",file?.path.toString())
+            cameralauncher.launch(intent)
+        }
+
+        binding.galleryBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            gallerylauncher.launch(intent)
+        }
+
         binding.publishBtn.setOnClickListener{
             val captionET = binding.captionET.text.toString()
 
            // Toast.makeText(activity,"Publicacion sin hacer", Toast.LENGTH_SHORT).show()
 
             listener?.let {
-                val publication = Publication("Juan Camilo",captionET,"2 marzo 2020","Cali, Colombia")
+                val myDate = Calendar.getInstance().time
+                val myString = DateFormat.getDateInstance().format(myDate);
+
+                val publication = Publication("Juan Camilo",captionET,myString,"Cali, Colombia")
                 it.onNewPublication(publication)
                 Toast.makeText(activity,"Publicacion hecha", Toast.LENGTH_SHORT).show()
             }
         }
 
         return view
+    }
+
+    fun onCameraResult(result: ActivityResult){
+       // val bitmap = result.data?.extras?.get("data") as Bitmap
+       // binding.photoIMG.setImageBitmap(bitmap)
+        if(result.resultCode == Activity.RESULT_OK){
+            val bitmap = BitmapFactory.decodeFile(file?.path)
+            binding.photoIMG.setImageBitmap(bitmap)
+        }else if(result.resultCode == Activity.RESULT_CANCELED){
+            Toast.makeText(activity,"No tomo la foto",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun onGalleryResult(result: ActivityResult){
+        if(result.resultCode == Activity.RESULT_OK){
+            val uriImage = result.data?.data
+            uriImage?.let {
+                binding.photoIMG.setImageURI(uriImage)
+            }
+
+        }
     }
 
     override fun onDestroyView() {
